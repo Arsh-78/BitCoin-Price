0f8dc5f0f1ea696a196e7c600f865334884a356f
Arsh-78 <arshbz53@gmail.com>
2022-06-21 03:36:57 +0530

    Got calls succesfully for Bitcoin

---
+++ lib/coin_data.dart
@@ -0,0 +1,44 @@
+import 'package:bitcoin_ticker/NetworkHelper.dart';
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+const apiKey = 'BEC75C86-3B32-48A3-B4F3-64ABF3DE53D7';
+
+const baseUrl = 'https://rest.coinapi.io/v1/exchangerate';
+
+class CoinData {
+  Future<dynamic> getCoinPrice() async {
+    var url = '$baseUrl/BTC/USD?apikey=$apiKey';
+    NetworkHelper networkHelper = NetworkHelper(url: url);
+    var coinData = await networkHelper.getCoinData();
+    return coinData;
+  }
+}
---
+++ lib/price_screen.dart
@@ -0,0 +1,128 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'coin_data.dart';
+
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  CoinData coinData = CoinData();
+  String selectedCurrency = 'USD';
+  String priceCur = '? USD';
+  String coinName = "BTC";
+
+  DropdownButton<String> androidPicker() {
+    List<DropdownMenuItem<String>> dropdownMenuItems = [];
+    for (String item in currenciesList) {
+      dropdownMenuItems.add(
+        DropdownMenuItem(
+          child: Text(item),
+          value: item,
+        ),
+      );
+    }
+
+    return DropdownButton(
+      value: selectedCurrency,
+      items: dropdownMenuItems,
+      onChanged: (value) {
+        setState(() {
+          selectedCurrency = value;
+        });
+      },
+    );
+  }
+
+  CupertinoPicker iosPicker() {
+    List<Text> pickermenuItems = [];
+    for (String item in currenciesList) {
+      pickermenuItems.add(Text(item));
+    }
+
+    return CupertinoPicker(
+      onSelectedItemChanged: (selectedIndex) {
+        print(selectedIndex);
+      },
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      children: pickermenuItems,
+    );
+  }
+
+/*  Widget getPicker() {
+    if (Platform.isIOS) {
+      return iosPicker();
+    } else {
+      return androidPicker();
+    }
+  } replaced with ternanry operator*/
+
+  void updateUI(dynamic coinData) {
+    setState(() {
+      if (coinData == null) {
+        print('Some Error');
+      }
+      double rate = coinData['rate'];
+      priceCur = rate.toStringAsFixed(2);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  void getData() async {
+    var coindata = await coinData.getCoinPrice();
+    updateUI(coindata);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('🤑 Coin Ticker'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 $coinName = $priceCur $selectedCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 150.0,
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(bottom: 30.0),
+            color: Colors.lightBlue,
+            child: Platform.isIOS ? iosPicker() : androidPicker(),
+          ),
+        ],
+      ),
+    );
+  }
+}
